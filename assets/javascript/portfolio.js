e8a5b1428c2459220b9a667bef61f79d8e07e88b
luis <hamburger120@outlook.com>
2019-01-15 18:44:28 -0500

    autoscroll

--- assets/javascript/portfolio.js
+++ assets/javascript/portfolio.js
@@ -1,8 +1,17 @@
- $("#button").click(function () {
-      // get the element on the page related to the button
-        var scrollToId = $(this).data("scroll");
-        var scrollToElement = document.getElementById(scrollToId);
-        // make the page scroll down to where you want
-        console.log(scrollToId);
-        // ...
+ $("#button1").click(function () {
+  //  event.preventdefault();
+  // get the element on the page related to the button
+  //   // var scrollToId = $(this).data("scroll");
+  //   $('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top}, 500, 'linear');
+  // make the page scroll down to where you want
+  //   console.log(scrollToId);
+  $([document.documentElement, document.body]).animate({
+    scrollTop: $("#footer").offset().top
+}, 1000);
     });
+    
+  $("#button2").click(function () {
+      $([document.documentElement, document.body]).animate({
+        scrollTop: $("#footer").offset().top
+    }, 1000);
+        });
